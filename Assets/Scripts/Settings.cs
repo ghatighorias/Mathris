693fad34d38a996cc8141c7de45d43e0e5c6bbed
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2019-02-22 17:16:44 +0100

    adds general settings for the game

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour {
+    public float fallDelay = 1F;
+    public bool allowAutomaticDrop = true;
+}
