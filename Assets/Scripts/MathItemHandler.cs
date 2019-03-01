2e75f0b19abe402d6e15e69cc2b428b81493b400
plangton <behrooz.mcsd@gmail.com>
2019-03-01 17:20:34 +0100

    add basic math prefabs

---
+++ Assets/Scripts/MathItemHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathItemHandler : MonoBehaviour {
+
+    public Sprite [] numbers;
+    public Sprite[] operators;
+
+    public Sprite GetRandomElement()
+    {
+        return null;
+    }
+}
