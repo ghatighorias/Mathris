c5deff3cc06af20fea4b78aa9d1577d574a36a57
plangton <behrooz.mcsd@gmail.com>
2019-03-01 16:11:30 +0100

    remove unused lines

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using static ShapeHandler;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(InputHandler))]
 public class Settings : MonoBehaviour {
